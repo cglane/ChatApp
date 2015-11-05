29bec02663b00b90e1166b904a89fb679aa321d6
Charles Lane <cglane@Charless-MacBook-Pro.local>
2015-11-05 17:00:22 -0500

    markup and event handlers

---
+++ client/templates.js
@@ -0,0 +1,22 @@
+
+var templates ={
+
+//id is unique id for message,
+//'span onload' starts the startTime() function on loaded.bs.modal
+//"clock" is where the time will be displayed
+  newMessage:[
+    '<li id = <%= id><p>',
+    '<span onload="startTime()>',
+    '<span id="clock"></span>',
+    '<%=message%>',
+    '</p></li>',
+  ].join(""),
+
+
+  users :[
+
+  ].join(""),
+
+};
+//
+//
